29e4e61664a29101eb524a83bb442cc669982f9b
anna <anna.kotanska@icloud.com>
2021-11-26 21:00:04 +0000

    Created a script with tests for pytest.

---
+++ test_pokemino_generator.py
@@ -0,0 +1,22 @@
+import pokemino_generator
+
+def test_pytest():
+    brick = pokemino_generator.Brick([1, 1], 1)
+    assert brick.pos == [1,1]
+
+def test_pokemino_of_required_size_created():
+    pass
+
+def test_clumped_algorithm_works():
+    pass
+
+def test_extended_algorithm_works():
+    pass
+
+def test_recenter_at_the_centre_of_mass():
+    pass
+
+def test_excluded_coords_correct():
+    pass
+
+
